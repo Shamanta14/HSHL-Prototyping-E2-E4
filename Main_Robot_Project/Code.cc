cefda1099091de23a4802b2cfd0d0ea8e76c6b39
Elvis-Ledoux <ngedimonelvis@gmail.com>
2026-05-31 03:00:57 +0200

    Add initial structure for robot control code

---
+++ Main_Robot_Project/Code.cc
@@ -0,0 +1,26 @@
+void setup() {
+  // IR sensor function code
+
+
+
+  // Ultra sonic sensor function code
+
+
+
+  // Motor driver function code
+
+}
+
+void loop() {
+
+  // IR sensor function call
+
+
+
+  // Ultra sonic sensor function call
+
+
+
+  // Motor driver function call
+
+}
